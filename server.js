eba04e505042f6f1190af70474ebae913045438c
02xjoe <johntobif@gmail.com>
2025-10-17 09:54:59 +0100

    d

--- server.js
+++ server.js
@@ -1,44 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import waitlistRoutes from './routes/waitlist.js';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const app = express();
-
-const allowedOrigins = ['https://lihaxa.netlify.app', 'http://localhost:5173'];
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type'],
-}));
 
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    serverSelectionTimeoutMS: 30000,
-    socketTimeoutMS: 45000,
-    family: 4,
-  })
-  .then(() => console.log("MongoDB connected successfully"))
-  .catch((err) => console.error("MongoDB connection error:", err));
 
-app.use(express.json());
-app.use('/api', waitlistRoutes);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-/*
 import dotenv from "dotenv";
 dotenv.config();
 console.log("Loaded .env file");
@@ -79,6 +41,3 @@ app.get("/", (req, res) => res.send("Lihaxa Backend is running"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-*/
